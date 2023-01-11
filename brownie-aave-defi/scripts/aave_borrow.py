2cff43d00bf7260385babd1639839640eb87774e
Lavishq <v.whitehearted@simplelogin.co>
2023-01-11 19:44:36 +0530

    able to borrow dai

--- brownie-aave-defi/scripts/aave_borrow.py
+++ brownie-aave-defi/scripts/aave_borrow.py
@@ -24,6 +24,35 @@ def main():
 
     # ...how much?
     borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+    print("let's borrow!")
+    # DAI in terms of ETH
+    dai_eth_price = get_asset_price(
+        config["networks"][network.show_active()]["dai_eth_price_feed"]
+    )
+    amount_dai_to_borrow = (1 / dai_eth_price) * (borrowable_eth * 0.95)
+    # borrowable_eth -> borrowable_dai * 95%\
+    print(f"We are going to borrow {amount_dai_to_borrow} DAI")
+    # Now we will borrow!
+    dai_address = config["networks"][network.show_active()]["dai_token"]
+    borrow_tx = lending_pool.borrow(
+        dai_address,
+        Web3.toWei(amount_dai_to_borrow, "ether"),
+        1,
+        0,
+        account.address,
+        {"from": account},
+    )
+    borrow_tx.wait(1)
+    print("We borrowed some DAI!")
+    get_borrowable_data(lending_pool, account)
+
+
+def get_asset_price(price_feed_address):
+    dai_eth_price_feed = interface.IAggregatorV3Interface(price_feed_address)
+    latest_price = dai_eth_price_feed.latestRoundData()[1]
+    converted_latest_price = Web3.fromWei(latest_price, "ether")
+    print(f"The DAI/ETH price is {converted_latest_price}")
+    return float(converted_latest_price)
 
 
 def get_borrowable_data(lending_pool, account):
