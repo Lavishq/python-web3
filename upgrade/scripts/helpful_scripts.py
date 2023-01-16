42f5cbdb2c921fba2cf58449356192540c1e60c8
Lavishq <v.whitehearted@simplelogin.co>
2023-01-16 15:39:32 +0530

    locally upgradeable contracts work

---
+++ upgrade/scripts/helpful_scripts.py
@@ -0,0 +1,81 @@
+from brownie import accounts, network, config
+import eth_utils
+
+# NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
+# LOCAL_BLOCKCHAIN_ENVIRONMENTS = NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS + [
+#     "mainnet-fork",
+#     "binance-fork",
+#     "matic-fork",
+# ]
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
+    if id:
+        return accounts.load()[id]
+    if network.show_active() in config["networks"]:
+        account = accounts.add(config["wallets"]["from_key"])
+        return account
+    return None
+
+
+def encode_function_data(initializer=None, *args):
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
+
+
+def upgrade(
+    account,
+    proxy,
+    newimplementation_address,
+    proxy_admin_contract=None,
+    initializer=None,
+    *args
+):
+    transaction = None
+    if proxy_admin_contract:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy_admin_contract.upgradeAndCall(
+                proxy.address,
+                newimplementation_address,
+                encoded_function_call,
+                {"from": account},
+            )
+        else:
+            transaction = proxy_admin_contract.upgrade(
+                proxy.address, newimplementation_address, {"from": account}
+            )
+    else:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy.upgradeToAndCall(
+                newimplementation_address, encoded_function_call, {"from": account}
+            )
+        else:
+            transaction = proxy.upgradeTo(newimplementation_address, {"from": account})
+    return transaction
+
+
+# def encode_function_data(initializer=None, *args):
+#     """Encodes the function call so we can work with an initializer.
+#     Args:
+#         initializer ([brownie.network.contract.ContractTx], optional):
+#         The initializer function we want to call. Example: `box.store`.
+#         Defaults to None.
+#         args (Any, optional):
+#         The arguments to pass to the initializer function
+#     Returns:
+#         [bytes]: Return the encoded bytes.
+#     """
+#     if not len(args): args = b''
+
+#     if initializer: return initializer.encode_input(*args)
+
+#     return b''
